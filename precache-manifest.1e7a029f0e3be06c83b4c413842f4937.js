72ad657eb9c87f0c19a2a9824794419e7e3f3056
jjasonlan <jjasonlan@berkeley.edu>
2021-06-12 14:20:30 -0700

    Initialize gh-pages

---
+++ precache-manifest.1e7a029f0e3be06c83b4c413842f4937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31127511f9355ef6c50fbf5b412aa8e1",
+    "url": "/CRA-gh-pages/index.html"
+  },
+  {
+    "revision": "5a6c5ba2b64d9707e6be",
+    "url": "/CRA-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fc40e406e313c8d1d0b",
+    "url": "/CRA-gh-pages/static/js/2.9dff47ea.chunk.js"
+  },
+  {
+    "revision": "5a6c5ba2b64d9707e6be",
+    "url": "/CRA-gh-pages/static/js/main.3e449574.chunk.js"
+  },
+  {
+    "revision": "f9f4145896fee745cf24",
+    "url": "/CRA-gh-pages/static/js/runtime-main.c583dc4c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CRA-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
